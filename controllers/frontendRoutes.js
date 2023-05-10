c3e4ddd84235cb361f479d6994f0946acf6c18d2
Tyler Eppley <teppley300@gmail.com>
2023-05-10 11:44:59 -0400

    added get route for login and signup

--- controllers/frontendRoutes.js
+++ controllers/frontendRoutes.js
@@ -7,4 +7,18 @@ router.get('/', async (req, res) => {
   res.render('homepage');
 });
 
+ //Get route for login page
+ router.get("/login", (req,res) =>{
+  if(req.session.user){
+    return res.redirect("/profile")
+  }
+  res.render("login")
+ })
+
+ //Get route for signup page
+ router.get("/signup", (req,res) =>{
+  res.render("signup")
+ })
+
+
 module.exports = router;
