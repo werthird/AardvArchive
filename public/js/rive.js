99f69c61566472073b3a8b75ba45f1c30a06fb58
jose torres <andytorres25995@gmail.com>
2023-05-12 11:37:11 -0400

    added aardvark to login page

--- public/js/rive.js
+++ public/js/rive.js
@@ -1,25 +1,13 @@
 
 new rive.Rive({
     src: "/assets/home.riv",
-    // Or the path to a public Rive asset
-    // src: '/public/example.riv',
     canvas: document.getElementById("canvas1"),
 });
  new rive.Rive({
     src: "/assets/profile.riv",
-    // Or the path to a public Rive asset
-    // src: '/public/example.riv',
     canvas: document.getElementById("canvas2"),
 });
  new rive.Rive({
     src: "/assets/logout.riv",
-    // Or the path to a public Rive asset
-    // src: '/public/example.riv',
     canvas: document.getElementById("canvas3"),
-});
-  new rive.Rive({
-    src: "/assets/newPost.riv",
-    // Or the path to a public Rive asset
-    // src: '/public/example.riv',
-    canvas: document.getElementById("canvas4"),
 });
